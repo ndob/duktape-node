cc27c5f373e5f530bf0af1ffa787df594bb57204
ndob <dobniki@gmail.com>
2015-09-12 18:28:10 +0300

    Initial changes to support 4.0.0.

--- src/run_async.cpp
+++ src/run_async.cpp
@@ -11,7 +11,6 @@ using namespace v8;
 
 namespace {
 
-using node::FatalException;
 using nan_addon::PersistentCallback;
 
 // Forward declaration for APICallbackSignaling destructor.
@@ -193,13 +192,13 @@ void onWorkDone(uv_work_t* req, int status)
 	argv[0] = Nan::New(work->hasError);
 	argv[1] = Nan::New(work->returnValue).ToLocalChecked();
 
-	TryCatch try_catch;
+	Nan::TryCatch try_catch;
 	auto callbackHandle = Nan::New(work->callback);
 	callbackHandle->Call(Nan::GetCurrentContext()->Global(), 2, argv);
 
 	if (try_catch.HasCaught()) 
 	{
-		FatalException(try_catch);
+		Nan::FatalException(try_catch);
 	}
 }
 
